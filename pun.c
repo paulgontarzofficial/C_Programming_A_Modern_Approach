3219c8cbf64f4ab6179cd3a32a30fc557f53a487
paulgontarzofficial <paulgontarz70@gmail.com>
2026-02-16 15:41:46 -0800

    Initializing the Repository

---
+++ pun.c
@@ -0,0 +1,14 @@
+/*
+ * Name: pun.c
+ * Purpose: Prints a bad pun.
+ * Author: K. N. King
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("To C, or not to C: ");
+	printf("that is the question.\n");
+	return 0;
+}
